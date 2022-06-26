fa58297fd81871ee93785dba5dbe37c27a581863
Marcos Oliveira <100429171+MarkGarOliver@users.noreply.github.com>
2022-06-26 12:37:58 -0300

    add route to register form

--- src/config/routes/index.tsx
+++ src/config/routes/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import LoginPage from '../../pages/auth/login';
+import RegisterPage from '../../pages/auth/register';
 
 // Pages
 import Home from '../../pages/home';
@@ -17,6 +18,7 @@ const Rotas = (props: Props) => {
             <Routes>
                 <Route path='/' element={<Home/>} />
                 <Route path='/login' element={<LoginPage/>  } />
+                <Route path='/register' element={<RegisterPage/>  } />
             </Routes>
         </BrowserRouter>
     );

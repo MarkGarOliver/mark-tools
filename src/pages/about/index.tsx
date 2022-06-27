6f1b10ff426ccd104849c9fa7100176f13930653
Marcos Oliveira <100429171+MarkGarOliver@users.noreply.github.com>
2022-06-27 14:54:25 -0300

    add pages

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,19 @@
+// @flow 
+import * as React from 'react';
+import { NavBar } from '../../components/partials/NavBar';
+type Props = {
+
+};
+const AboutPage = (props: Props) => {
+    return (
+        <>
+            <NavBar />
+
+            <div>
+                AboutPage
+            </div>
+        </>
+    );
+};
+
+export default AboutPage;
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,18 @@
+// @flow 
+import * as React from 'react';
+import { NavBar } from '../../components/partials/NavBar';
+type Props = {
+    
+};
+const ContactPage = (props: Props) => {
+    return (
+    <>
+        <NavBar/>
+        <div>
+            Contact page
+        </div>
+    </>
+    );
+};
+
+export default ContactPage;
